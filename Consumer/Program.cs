c9bd3c70047136e4ce79bec83dd3317ac21bd9d0
Nestor Mauricio Pérez Ruíz <nestormauricio@live.com>
2025-11-21 16:37:51 -0500

    Se tuvo que instalar RabbitMQ.Client 5.2.0. porque la versión 7 no era compatible

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -1,2 +1,122 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Esperando mensajes...");
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost"
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "demo-queue",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($"[x] Recibido: {message}");
+};
+
+channel.BasicConsume(
+    queue: "demo-queue",
+    autoAck: true,
+    consumer: consumer
+);
+
+Console.WriteLine("Presiona ENTER para salir");
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// using RabbitMQ.Client;
+// using RabbitMQ.Client.Events;
+// using System.Text;
+
+// Console.WriteLine("Esperando mensajes...");
+
+// var factory = new ConnectionFactory()
+// {
+//     HostName = "localhost"
+// };
+
+// using var connection = factory.CreateConnection();
+// using var channel = connection.CreateModel();
+
+// // Declaramos la misma cola
+// channel.QueueDeclare(queue: "demo-queue",
+//                      durable: false,
+//                      exclusive: false,
+//                      autoDelete: false,
+//                      arguments: null);
+
+// // Creamos el consumidor
+// var consumer = new EventingBasicConsumer(channel);
+
+// consumer.Received += (model, ea) =>
+// {
+//     var body = ea.Body.ToArray();
+//     var message = Encoding.UTF8.GetString(body);
+//     Console.WriteLine($"[x] Recibido: {message}");
+// };
+
+// // Consumimos mensajes
+// channel.BasicConsume(queue: "demo-queue",
+//                      autoAck: true,
+//                      consumer: consumer);
+
+// // Mantenemos la app corriendo
+// Console.WriteLine("Presiona [ENTER] para salir.");
+// Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Enviando mensaje a RabbitMQ...");
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost"
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+// Declaramos la cola donde enviaremos mensajes
+channel.QueueDeclare(queue: "demo-queue",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+string message = "Hola desde el Producer!";
+var body = Encoding.UTF8.GetBytes(message);
+
+// Publicamos el mensaje
+channel.BasicPublish(exchange: "",
+                     routingKey: "demo-queue",
+                     basicProperties: null,
+                     body: body);
+
+Console.WriteLine($"[x] Enviado: {message}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
